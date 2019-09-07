c74c0f2b21dbe767e5b2672875c882c36086f4c5
Khanson3 <khanson3@binghamton.edu>
2019-09-07 18:59:34 -0400

    Selecting an input unit now restricts the selectable output units to related unit (length units, weight units, temperature units, etc.). Output value changed to disabled so it's not selectable after inputting input value. Slight design modifications to spinners.

--- app/src/main/java/edu/binghamton/khanson3/unitconverter/MainActivity.java
+++ app/src/main/java/edu/binghamton/khanson3/unitconverter/MainActivity.java
@@ -1,8 +1,9 @@
 package edu.binghamton.khanson3.unitconverter;
 
+import android.content.res.Resources;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,18 +17,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Resources res = getResources();
+        String[] lengthUnits = res.getStringArray(R.array.length_units);
+        String[] weightUnits = res.getStringArray(R.array.weight_units);
+        String[] temperatureUnits = res.getStringArray(R.array.temperature_units);
+        String[] speedUnits = res.getStringArray(R.array.speed_units);
+        String[] timeUnits = res.getStringArray(R.array.time_units);
+        String[] volumeUnits = res.getStringArray(R.array.volume_units);
+        final String[][] allUnits = {lengthUnits, weightUnits, temperatureUnits, speedUnits, timeUnits, volumeUnits};
+
         TextView outputTextView = findViewById(R.id.outputTextView);
-        outputTextView.setKeyListener(null);
+        //outputTextView.setKeyListener(null);
+        outputTextView.setEnabled(false);
 
         Spinner inputSpinner = findViewById(R.id.inputSpinner);
-        ArrayAdapter<CharSequence> allUnits = ArrayAdapter.createFromResource(this, R.array.all_units, android.R.layout.simple_spinner_item);
-        allUnits.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        inputSpinner.setAdapter(allUnits);
+        ArrayAdapter<CharSequence> inputUnits = ArrayAdapter.createFromResource(this, R.array.all_units, android.R.layout.simple_spinner_item);
+        inputUnits.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        inputSpinner.setAdapter(inputUnits);
+
+        final Spinner outputSpinner = findViewById(R.id.outputSpinner);
 
         inputSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String unit = ((TextView) adapterView.getChildAt(0)).getText().toString();
                 ((TextView) adapterView.getChildAt(0)).setTextColor(Color.WHITE);
+                ((TextView) adapterView.getChildAt(0)).setTextSize(18.65f);
+
+                for(int j = 0; j < allUnits.length; j++) {
+                    for(String elem : allUnits[j]) {
+                        if(elem.equals(unit)) {
+                            int strArrayID;
+                            if(j == 0)
+                                strArrayID = R.array.length_units;
+                            else if(j == 1)
+                                strArrayID = R.array.weight_units;
+                            else if(j == 2)
+                                strArrayID = R.array.temperature_units;
+                            else if(j == 3)
+                                strArrayID = R.array.speed_units;
+                            else if(j == 4)
+                                strArrayID = R.array.time_units;
+                            else
+                                strArrayID = R.array.volume_units;
+
+                            ArrayAdapter<CharSequence> outputUnits = ArrayAdapter.createFromResource(getApplicationContext(), strArrayID, android.R.layout.simple_spinner_item);
+                            outputUnits.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            outputSpinner.setAdapter(outputUnits);
+
+                            outputSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                                    ((TextView) adapterView.getChildAt(0)).setTextColor(Color.WHITE);
+                                    ((TextView) adapterView.getChildAt(0)).setTextSize(18.65f);
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                                }
+                            });
+                        }
+                    }
+                }
             }
 
             @Override
@@ -35,5 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
