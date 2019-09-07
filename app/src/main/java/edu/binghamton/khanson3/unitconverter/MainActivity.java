b9ded5c59f1b315306ac18a04f1276ae806714ec
Khanson3 <khanson3@binghamton.edu>
2019-09-07 16:09:44 -0400

    Added all units to spinner dropdown menu.

--- app/src/main/java/edu/binghamton/khanson3/unitconverter/MainActivity.java
+++ app/src/main/java/edu/binghamton/khanson3/unitconverter/MainActivity.java
@@ -1,7 +1,13 @@
 package edu.binghamton.khanson3.unitconverter;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView outputTextView = findViewById(R.id.outputTextView);
+        outputTextView.setKeyListener(null);
+
+        Spinner inputSpinner = findViewById(R.id.inputSpinner);
+        ArrayAdapter<CharSequence> allUnits = ArrayAdapter.createFromResource(this, R.array.all_units, android.R.layout.simple_spinner_item);
+        allUnits.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        inputSpinner.setAdapter(allUnits);
+
+        inputSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView) adapterView.getChildAt(0)).setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
